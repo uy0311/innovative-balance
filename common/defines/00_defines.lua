8046f73e24afe63c52664362e3c9ac6f36d72a8c
uy0311 <u.y.031155555@gmail.com>
2022-08-19 02:08:56 +0900

    8/19

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1619,9 +1619,9 @@ NNavy = {
 	LIGHT_GUN_ATTACK_TO_SHORE_BOMBARDMENT							= 0.05, -- light gun attack value is divided by this value * 100 and added to shore bombardment modifier
 
 	GUN_HIT_PROFILES = { -- hit profiles for guns, if target ih profile is lower the gun will have lower accuracy
-		113.0,	-- big guns
+		108.0,	-- big guns
 		205.0,	-- torpedos
-		127.0,	-- small guns
+		122.0,	-- small guns
 	},
 
 	DEPTH_CHARGES_HIT_CHANCE_MULT 									= 1.1, 		-- multiplies hit chance of small guns
