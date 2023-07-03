81c20de722c54cef03058a149cd756b061748819
yukikuu <yukikuu1203@gmail.com>
2023-07-03 23:50:59 +0900

    -defines ・航空機撃墜率を0.55から0.75に ・空戦射撃上限機数を2から1に
    -空軍兵装の変更
    ・シート参照

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1153,8 +1153,8 @@ NAir = {
 	COMBAT_MAX_WINGS_AT_GROUND_ATTACK = 1000,	        	-- we can really pounce a land strike and escalate
 	COMBAT_MAX_WINGS_AT_ONCE_PORT_STRIKE = 1000,        -- we can really pounce a naval strike and escalate
 	AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.04,           -- air superiority scale = superiority/(pixels*this)
-	COMBAT_MULTIPLANE_CAP = 2.0,						-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
-	COMBAT_DAMAGE_SCALE = 0.55,							-- Higher value = more shot down planes
+	COMBAT_MULTIPLANE_CAP = 1.0,						-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
+	COMBAT_DAMAGE_SCALE = 0.75,							-- Higher value = more shot down planes
 	COMBAT_DAMAGE_SCALE_CARRIER = 0,					-- same as above but used inside naval combat for carrier battles
 	DETECT_CHANCE_FROM_OCCUPATION = 0.10, 				-- How much the controlled provinces in area affects the air detection base value.
 	DETECT_CHANCE_FROM_RADARS = 0.5, 					-- How much the radars in area affects detection chance.
